588700a594f84699c9f11712530112e36e06c161
namastayinschool <tmalika96@hotmail.com>
2017-03-15 11:44:06 -0400

    Implemented StackV

--- StackV.cpp
+++ StackV.cpp
@@ -1,1 +1,34 @@
 #include "StackV.h"
+#include <iostream>
+using namespace std;
+
+int Stack::size()
+{
+	return data.size();
+}
+
+void Stack::push(int num)
+{
+	data.push_back(num);
+}
+
+void Stack::pop()
+{
+	data.pop_back();
+}
+
+int Stack::top()
+{
+	if (data.back() > -1)
+	{
+		return data.back();
+	}
+	else
+		cerr <<"Stack is empty";
+}
+
+void Stack::clear()
+{
+	data.clear();
+}
+	
