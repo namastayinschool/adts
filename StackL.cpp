57202aec5ddcd8509bb129c6b5cdf730121f12b0
namastayinschool <tmalika96@hotmail.com>
2017-03-16 02:06:38 -0400

    Stack ll code written

--- StackL.cpp
+++ StackL.cpp
@@ -1,1 +1,34 @@
 #include "StackL.h"
+#include <iostream>
+using namespace std;
+
+int Stack::size()
+{
+	return data.size();
+}
+
+void Stack::push(int num)
+{
+	data.push_back(num)
+}
+
+void Stack::pop_back;
+
+{
+	data.remove;
+}
+
+int Stack::top()
+{
+	if (data.back() > -1)
+	{
+		return data.back();
+	}
+	else
+		cerr <<"Stack is empty";
+}
+
+void Stack::clear()
+{
+	data.clear();
+}
